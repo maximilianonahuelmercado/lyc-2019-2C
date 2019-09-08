bb5ad7a7c2229e86675b76447f71c481548cfc27
Maximiliano <maximilianomercado.n@gmail.com>
2019-09-08 15:06:39 -0300

    Lexico

---
+++ files_h/constantes.h
@@ -0,0 +1,22 @@
+
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+// Cotas para el lexico
+#define COTA_INT 32767
+#define COTA_FLOAT 3.402823E+38
+#define COTA_STR 32
+#define COTA_ID 30
+
+// Existencia de token
+#define EXISTE 1
+#define NO_EXISTE 0
+
+// Variable para debug
+#define DEBUG 0
+
+// Manejo de errores
+#define TODO_OK 1
+#define ERROR 0
+
+#endif
